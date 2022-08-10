f671b9b28eaded652f76b50809af1a29bc6c5ee0
Pooja28Madankar <87806904+Pooja28Madankar@users.noreply.github.com>
2022-08-10 12:16:54 +0530

    Add files via upload

---
+++ blank/connect.php
@@ -0,0 +1,23 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','cj');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into form(firstName, lastName, gender, email, password, number) values(pooja, madankar, female, pooja28madankar2gmail.com, Pooja_123, 7745892318)");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
